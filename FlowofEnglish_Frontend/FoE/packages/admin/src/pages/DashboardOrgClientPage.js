e46fd93bbeae9ad2e3f040529187c6fd235d6ed5
Harikrishna513 <harikrishna055hari@gmail.com>
2024-11-15 19:49:31 +0530

    15/11/24

--- FlowofEnglish_Frontend/FoE/packages/admin/src/pages/DashboardOrgClientPage.js
+++ FlowofEnglish_Frontend/FoE/packages/admin/src/pages/DashboardOrgClientPage.js
@@ -3,6 +3,8 @@ import { useNavigate, useParams, Link as RouterLink } from 'react-router-dom';
 import axios from 'axios';
 import { Helmet } from 'react-helmet-async';
 import { Grid, Container, Typography, Box, Button, Card, CardContent, CardHeader } from '@mui/material';
+import { AppWidgetSummary, AppNewsUpdate } from '../sections/@dashboard/app';
+
 import { getOrgCohorts, getOrgUsers } from '../api';
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -49,69 +51,122 @@ export default function DashboardOrgClientPage() {
         </Typography>
 
         <Grid container spacing={3}>
-          {/* Organization Card */}
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardHeader title="Organization Info" />
-              <CardContent>
-                <Typography variant="h6">Organization: {orgData.organizationName}</Typography>
-                <Typography variant="body2">OrganizationId: {orgData.organizationId}</Typography>
-                <Typography variant="body2">Admin: {orgData.organizationAdminName}</Typography>
-                <Typography variant="body2">Email: {orgData.organizationAdminEmail}</Typography>
-                <Typography variant="body2">Phone: {orgData.organizationAdminPhone}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
 
-          {/* Users Card */}
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardHeader title="Users" />
-              <CardContent>
-                <Typography variant="h6">Total Users: {users.length}</Typography>
-                <Box display="flex" justifyContent="flex-end">
-                  <Button component={RouterLink} to={`/org-dashboards/${id}/org-Create-Users`} variant="outlined" size="large" color="primary">
-                    View All Users
-                  </Button>
-                </Box>
-              </CardContent>
-            </Card>
+          {/* Learners Card */}
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary
+              title="Learners"
+              total={users.length}
+              icon={'ant-design:user-outlined'}
+            />
           </Grid>
 
           {/* Cohorts Card */}
-          <Grid item xs={12} sm={6} md={4}>
-            <Card>
-              <CardHeader title="Cohorts" />
-              <CardContent>
-                <Typography variant="h6">Total Cohorts: {cohorts.length}</Typography>
-                <Box display="flex" justifyContent="flex-end">
-                  <Button component={RouterLink} to={`/org-dashboards/${id}/orgdashc`} variant="outlined" size="large" color="primary">
-                    View All Cohorts
-                  </Button>
-                  </Box>
-              </CardContent>
-            </Card>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary
+              title="Cohorts"
+              total={cohorts.length}
+              color="info"
+              icon={'ant-design:whats-app-outlined'}
+            />
+            </Grid>
+            {/* Programs Card */}
+           <Grid item xs={12} sm={6} md={3}>
+            <AppWidgetSummary title="Programs" 
+            total={cohorts.length} 
+            color="error" 
+            icon={'ant-design:flag-outlined'}
+             />
           </Grid>
-
-          {/* Users List Card */}
-          <Grid item xs={12}>
-            <Card>
-              <CardHeader title="Registered Users" />
-              <CardContent>
-                {users.length > 0 ? (
-                  users.map((user) => (
-                    <Typography key={user.userId}>
-                      {user.userName} - {user.userPhoneNumber}
-                    </Typography>
-                  ))
-                ) : (
-                  <Typography>No registered users available</Typography>
-                )}
-              </CardContent>
-            </Card>
+          {/* Registered Learners Update */}
+          <Grid item xs={12} md={6} lg={8}>
+            <AppNewsUpdate
+              title="Registered Learners"
+              list={users.map((user, index) => ({
+                id: index,
+                title: user.userName,
+                description: user.userPhoneNumber,
+                image: `/assets/images/covers/cover_1.jpg`,
+              }))}
+            />
           </Grid>
-        </Grid>
+          </Grid>
       </Container>
     </>
   );
 }
+
+           
+          //    View All Buttons 
+
+          //  <Grid item xs={12} md={6} lg={4}>
+          //   <Button
+          //     fullWidth
+          //     component={RouterLink}
+          //     to={`/org-dashboards/${id}/org-Create-Users`}
+          //     variant="outlined"
+          //     color="primary"
+          //     size="large"
+          //     sx={{ mb: 2 }}
+          //   >
+          //     View All Learners
+          //   </Button>
+          //   <Button
+          //     fullWidth
+          //     component={RouterLink}
+          //     to={`/org-dashboards/${id}/orgdashc`}
+          //     variant="outlined"
+          //     color="primary"
+          //     size="large"
+          //   >
+          //     View All Cohorts
+          //   </Button>
+          //   </Grid> 
+          
+   
+          // <Grid item xs={12} sm={6} md={4}>
+          //   <Card>
+          //     <CardHeader title="Learners" />
+          //     <CardContent>
+          //       <Typography variant="h6">Total Learners: {users.length}</Typography>
+          //       <Box display="flex" justifyContent="flex-end">
+          //         <Button component={RouterLink} to={`/org-dashboards/${id}/org-Create-Users`} variant="outlined" size="large" color="primary">
+          //           View All Learners
+          //         </Button>
+          //       </Box>
+          //     </CardContent>
+          //   </Card>
+          // </Grid>
+          
+          // <Grid item xs={12} sm={6} md={4}>
+          //   <Card>
+          //     <CardHeader title="Cohorts" />
+          //     <CardContent>
+          //       <Typography variant="h6">Total Cohorts: {cohorts.length}</Typography>
+          //       <Box display="flex" justifyContent="flex-end">
+          //         <Button component={RouterLink} to={`/org-dashboards/${id}/orgdashc`} variant="outlined" size="large" color="primary">
+          //           View All Cohorts
+          //         </Button>
+          //         </Box>
+          //     </CardContent>
+          //   </Card>
+          // </Grid>
+          
+          // <Grid item xs={12}>
+          //   <Card>
+          //     <CardHeader title="Registered Learners" />
+          //     <CardContent>
+          //       {users.length > 0 ? (
+          //         users.map((user) => (
+          //           <Typography key={user.userId}>
+          //             {user.userName} - {user.userPhoneNumber}
+          //           </Typography>
+          //         ))
+          //       ) : (
+          //         <Typography>No registered Learners available</Typography>
+          //       )}
+          //     </CardContent>
+          //   </Card>
+          // </Grid> 
+          
+        
