8bf90b5f3374a855e9539bf92bf020499fcec3dd
Harikrishna513 <harikrishna055hari@gmail.com>
2024-11-24 11:42:41 +0530

    24/11/2024

--- FlowofEnglish_Backend/src/main/java/com/FlowofEnglish/dto/CohortProgressDTO.java
+++ FlowofEnglish_Backend/src/main/java/com/FlowofEnglish/dto/CohortProgressDTO.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class CohortProgressDTO {
     private String programName;
+    private String programId;
+    private String programDesc;
+    private String cohortId;
     private String cohortName;
     private List<UserProgressDTO> users;
     
@@ -13,6 +16,24 @@ public class CohortProgressDTO {
 	}
 	public void setProgramName(String programName) {
 		this.programName = programName;
+	}
+	public String getProgramId() {
+		return programId;
+	}
+	public void setProgramId(String programId) {
+		this.programId = programId;
+	}
+	public String getProgramDesc() {
+		return programDesc;
+	}
+	public void setProgramDesc(String programDesc) {
+		this.programDesc = programDesc;
+	}
+	public String getCohortId() {
+		return cohortId;
+	}
+	public void setCohortId(String cohortId) {
+		this.cohortId = cohortId;
 	}
 	public String getCohortName() {
 		return cohortName;
