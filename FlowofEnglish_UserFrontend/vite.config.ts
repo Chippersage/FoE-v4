4a6b536fe4f8807dd5a446a9113805e2b011d887
NiRaJsHa91 <yoniraj91@gmail.com>
2024-12-27 19:15:52 +0530

    user-frontend-27/12/24

--- FlowofEnglish_UserFrontend/vite.config.ts
+++ FlowofEnglish_UserFrontend/vite.config.ts
@@ -1,27 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  // base: "./",
   plugins: [react()],
-  build: {
-    target: "modules",
-    outDir: path.resolve(__dirname, "./dist"),
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
