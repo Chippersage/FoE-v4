6ae993d0f3ebe7bbcd6fba91a4f6f34bb7084617
NiRaJsHa91 <yoniraj91@gmail.com>
2025-02-14 19:56:02 +0530

    only dynamic height calculation of viewbox of svg left in scurve responsiveness

--- FlowofEnglish_UserFrontend/src/components/MediaContent.tsx
+++ FlowofEnglish_UserFrontend/src/components/MediaContent.tsx
@@ -511,7 +511,7 @@ const MediaContent = ({ subconceptData }) => {
             
             <button
               onClick={handleGoBack}
-              className="bg-[#00A66B] hover:bg-green-600 text-white px-3 py-2 sm:px-4 sm:py-3 m-1 sm:m-2 rounded-md text-sm sm:text-base md:text-lg transition-all max-w-[150px] sm:max-w-[200px]"
+              className="bg-[#00A66B] hover:bg-green-600 text-white px-3 py-1 sm:px-4 sm:py-3 m-1 sm:m-2 rounded-[2px] text-sm sm:text-base md:text-lg transition-all max-w-[150px] sm:max-w-[200px]"
             >
               Go Back
             </button>
