da9d06446f60b12bb3a2b7ebd5e5c437f011a3f6
Harikrishna513 <harikrishna055hari@gmail.com>
2025-04-21 19:43:27 +0530

    @transactional rollback updated in the pgm

---
+++ FlowofEnglish_Backend/src/main/java/com/FlowofEnglish/exception/BulkUploadException.java
@@ -0,0 +1,8 @@
+package com.FlowofEnglish.exception;
+
+public class BulkUploadException extends RuntimeException {
+    public BulkUploadException(String message) {
+        super(message);
+    }
+}
+
