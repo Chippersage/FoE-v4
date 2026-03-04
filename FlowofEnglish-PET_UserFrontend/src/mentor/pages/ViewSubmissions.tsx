5363b3b9cf10b96e59ae79cf1d6c43d32ffed674
Shajad Shaik <shajadshaik36@gmail.com>
2026-03-04 15:34:19 +0530

    Minor corrections of name changing

--- FlowofEnglish-PET_UserFrontend/src/mentor/pages/ViewSubmissions.tsx
+++ FlowofEnglish-PET_UserFrontend/src/mentor/pages/ViewSubmissions.tsx
@@ -1358,7 +1358,7 @@ const ViewSubmissions: React.FC = () => {
                                 >
                                   <div className="flex items-center justify-between mb-4">
                                     <h4 className="font-medium text-gray-800 text-sm">
-                                      {isCorrected ? 'Evaluation Details' : 'Correct Assignment'}
+                                      {isCorrected ? 'Evaluation Details' : 'Evaluate Assignment'}
                                     </h4>
                                     {assignment.subconcept?.subconceptDesc2?.trim() && (
                                       isAudioSubmission ? (
