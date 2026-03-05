bdab084118a36b76b17af65151cb46035571ae60
KURUVA HARIKRISHNA <132289116+Harikrishna513@users.noreply.github.com>
2026-03-05 19:26:59 +0530

    the task is failing so for testing that pushing again

--- FlowofEnglish_Backend/src/main/java/com/FlowofEnglish/FlowofEnglishBackendApplication.java
+++ FlowofEnglish_Backend/src/main/java/com/FlowofEnglish/FlowofEnglishBackendApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoCo
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
 @SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = "com.FlowofEnglish.repository")
 @EnableTransactionManagement
